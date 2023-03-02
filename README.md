fee5399cea6a2ad57f0ff3c2ee9335877370be14
Sean Hagen <49661807+HagenSR@users.noreply.github.com>
2023-03-02 10:28:20 -0600

    Added ability to provide input Added readme Fixed bug with client file detection refactored to conform to snake case standard

---
+++ README.md
@@ -0,0 +1,23 @@
+# Java auto runner
+
+## Why
+
+This script was made to help facilitate the grading of 161 projects. The professor had students turn in zips of java source code along with a word doc containing the source code. This project unzips the source code, compiles it, runs it and stores it's output in a file. Running the code is faster and more foolproof way of determining if the code is correct. Additionally, the source code can be viewed in VS Code with language support which helps highlight errors in contrast with reading code from a word doc. 
+
+## How to use
+
+Because this script involves arbitrarily executing code student's give you, it is advisable to run this script in a virtual machine. 
+
+1. Put all zips in a folder called 'zips' in the same directory as the script. Other files can exist in this folder however only zips will be compiled.
+2. Optionally provide input in the 'input.in' file. The projects will receive this as standard input.
+3. Optionally change the timeout duration in the script on line 125. The duration is in seconds.
+4. run the command `python run_zips.py`
+5. After the script has completed running, output for each project can be found in the 'out' folder
+6. Source code can be viewed in the build folder
+
+## Features
+
+- Project's that run beyond a set timeout duration will be killed
+- All output including standard error is written to the output file
+- Project's with varying client class names and packages will be run correctly
+- Ability to provide input to the project being run
--- run_zips.py
+++ run_zips.py
@@ -8,18 +8,17 @@ from subprocess import STDOUT, PIPE
 from multiprocessing import Process
 
 
-class zipRunner:
+class ZipRunner:
 
-    def __init__(self, zipDir=".\\zips", buildDir=".\\build", outputDir=".\\out", stdIn=None, maxTime=200) -> None:
-        self.zipDir = zipDir
-        self.buildDir = buildDir
-        self.createOrRemove(self.buildDir)
-        self.outputDir = outputDir
-        self.createOrRemove(self.outputDir)
-        self.stdIn = stdIn
-        self.MAX_TIMEOUT = maxTime
+    def __init__(self, zip_dir=".\\zips", build_dir=".\\build", output_dir=".\\out", max_time=100) -> None:
+        self.zip_dir = zip_dir
+        self.build_dir = build_dir
+        self.create_or_remove(self.build_dir)
+        self.output_dir = output_dir
+        self.create_or_remove(self.output_dir)
+        self.MAX_TIMEOUT = max_time
 
-    def createOrRemove(self, folder):
+    def create_or_remove(self, folder):
         if os.path.exists(folder):
             shutil.rmtree(folder)
         os.mkdir(folder)
@@ -29,17 +28,17 @@ class zipRunner:
         self.build()
 
     def unzip(self):
-        zips = glob.glob(f"{self.zipDir}/*.zip")
+        zips = glob.glob(f"{self.zip_dir}/*.zip")
         for zip in zips:
-            with zipfile.ZipFile(zip, "r") as zipref:
-                fileName = zip.split("\\")[-1].lower().replace(".zip", "")
-                zipref.extractall(f"{self.buildDir}/{fileName}")
+            with zipfile.ZipFile(zip, "r") as zip_ref:
+                file_name = zip.split("\\")[-1].lower().replace(".zip", "")
+                zip_ref.extractall(f"{self.build_dir}/{file_name}")
 
     def build(self):
-        dirs = os.listdir(self.buildDir)
+        dirs = os.listdir(self.build_dir)
         for dir in dirs:
             print(f"Starting {dir}")
-            fileName = dir.split("\\")[-1].lower()
+            file_name = dir.split("\\")[-1].lower()
             try:
                 proc = Process(
                     target=self.build_internal, args=[dir]
@@ -50,8 +49,8 @@ class zipRunner:
                     raise TimeoutError()
             except TimeoutError:
                 proc.kill()
-                print(f"{fileName}: Did not complete in time")
-                self.write_to_file(fileName, "failed to finish task in time")
+                print(f"{file_name}: Did not complete in time")
+                self.write_to_file(file_name, "failed to finish task in time")
 
     def build_internal(self, dir):
         try:
@@ -60,6 +59,7 @@ class zipRunner:
             self.execute_java(dir, package)
         except Exception as e:
             print(f"{dir}: {str(e)}")
+            self.write_to_file(dir, str(e))
 
     def compile_java(self, dir, src_dir, java_file):
         cmd = " ".join(['javac', '-d', f'build/{dir}/out/',
@@ -76,26 +76,27 @@ class zipRunner:
     def execute_java(self, fileName, package):
         cmd = " ".join(
             ['java', '-cp', f'.\\build\\{fileName}\\out\\.', f'{package}'])
+        st = open("input.in")
         proc = subprocess.Popen(
-            cmd, stdin=PIPE, stdout=PIPE, stderr=STDOUT, shell=False)
-        stdout, stderr = proc.communicate(self.stdIn)
+            cmd, stdin=st, stdout=PIPE, stderr=STDOUT, shell=False)
+        stdout, stderr = proc.communicate()
         self.write_to_file(fileName, stdout if stdout is not None else stderr)
 
     def write_to_file(self, filename, res):
         if type(res) is str:
             res = bytes(res, "utf-8")
-        with open(f".\\{self.outputDir}\\{filename}.txt", "wb+") as fl:
+        with open(f".\\{self.output_dir}\\{filename}.txt", "ab+") as fl:
             fl.write(res)
 
     def get_build_info(self, dir):
         parts = []
-        file, contents = self.findClientFile(dir)
+        file, contents = self.find_client_file(dir)
         jv_class = "Client"
         for part in file.split("\\"):
             parts.append(part)
             if part == "src":
                 break
-        package = "Client"
+        package = ""
         for line in contents.split("\n"):
             if "public class " in line:
                 jv_class = line.removeprefix(
@@ -103,9 +104,13 @@ class zipRunner:
             if "package" in line:
                 package = line.removeprefix(
                     "package").removesuffix(";").strip()
-        return "\\".join(parts), file, package + f".{jv_class}"
+        if package == "":
+            return "\\".join(parts), file, jv_class
+        else:
+            return "\\".join(parts), file, package + f".{jv_class}"
 
-    def findClientFile(self, dir):
+
+    def find_client_file(self, dir):
         files = [os.path.join(dp, f) for dp, dn, fn in os.walk(
             os.path.expanduser(f"./build/{dir}")) for f in fn]
         for file in files:
@@ -118,5 +123,5 @@ class zipRunner:
 
 
 if __name__ == "__main__":
-    run = zipRunner(stdIn=b"100")
+    run = ZipRunner(max_time=100)
     run.run()
